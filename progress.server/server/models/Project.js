d560bd02f67b40e4be22b9cb8268d1849ca3b320
davidparker83686 <davidjparker91@gmail.com>
2021-06-23 23:34:26 -0600

    boolean

--- progress.server/server/models/Project.js
+++ progress.server/server/models/Project.js
@@ -5,7 +5,7 @@ const Project = new Schema(
   {
     title: { type: String, required: true },
     description: { type: String, required: true },
-    favorite: { type: Boolean, required: false, default: false },
+    favorite: { type: Boolean, required: true, default: false },
     creatorId: { type: String, ref: 'Account', required: true }
   },
   { timestamps: true, toJSON: { virtuals: true } }
